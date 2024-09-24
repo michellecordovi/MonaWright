d66690d2d014f7d9a4a205e94f453ec4af8144ac
MichelleCordovi <michellecordovi@gmail.com>
2024-09-24 14:38:45 -0400

    create gero section

--- Client/src/Pages/Home/HomePage.jsx
+++ Client/src/Pages/Home/HomePage.jsx
@@ -1,5 +1,19 @@
+import './HomePage.css'
+
 function HomePage() {
-	return <h1>Home Page!</h1>;
+	return (
+		<header className="hero overlay">
+			<div id="hero-text">
+				<h1>Mona Wright</h1>
+				<p>
+					Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
+					magna tempor velit laoreet massa vivamus nulla. Praesent
+					habitant suscipit vel nisl vehicula orci. Natoque senectus
+					ridiculus sit proin orci molestie a.
+				</p>
+			</div>
+		</header>
+	);
 }
 
 export default HomePage;
