1fc8051831cff1e8afd7b567243187702d6a5913
MichelleCordovi <michellecordovi@gmail.com>
2024-09-24 13:13:14 -0400

    create responsive mobile navbar feature

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,21 +1,41 @@
 import {NavLink} from 'react-router-dom'
+import {useState} from 'react'
 
 function NavBar() {
+	const [isExpanded, setIsExpanded] = useState(false)
+
+	function handleToggle(){
+		setIsExpanded(!isExpanded)
+	}
+
+
 	return (
 		<nav>
 			<div id="site-logo">Mona Wright</div>
 
-			<ul>
-				<li>
-					<NavLink to="/" >Home</NavLink>
+			<ul className={isExpanded ? 'expanded' : ''}>
+				<li onClick={handleToggle}>
+					<NavLink to="/">Home</NavLink>
 				</li>
-				<li>
+				<li onClick={handleToggle}>
 					<NavLink to="/About">About</NavLink>
 				</li>
-				<li>
+				<li onClick={handleToggle}>
 					<NavLink to="Poetry">Poetry</NavLink>
 				</li>
 			</ul>
+
+			<div id="menu-icon" onClick={handleToggle}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					height="24px"
+					viewBox="0 -960 960 960"
+					width="24px"
+					fill="#fdfee8"
+				>
+					<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+				</svg>
+			</div>
 		</nav>
 	);
 }
