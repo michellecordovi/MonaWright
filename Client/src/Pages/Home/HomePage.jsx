59851c74d2ebd9219945f05fec0123c60088978d
MichelleCordovi <michellecordovi@gmail.com>
2024-09-24 15:20:41 -0400

    create about section

--- Client/src/Pages/Home/HomePage.jsx
+++ Client/src/Pages/Home/HomePage.jsx
@@ -2,17 +2,58 @@ import './HomePage.css'
 
 function HomePage() {
 	return (
-		<header className="hero overlay">
-			<div id="hero-text">
-				<h1>Mona Wright</h1>
-				<p>
-					Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
-					magna tempor velit laoreet massa vivamus nulla. Praesent
-					habitant suscipit vel nisl vehicula orci. Natoque senectus
-					ridiculus sit proin orci molestie a.
-				</p>
-			</div>
-		</header>
+		<>
+			<header className="hero overlay">
+				<div id="hero-text">
+					<h1>Mona Wright</h1>
+					<p>
+						Lorem ipsum odor amet, consectetuer adipiscing elit.
+						Tortor magna tempor velit laoreet massa vivamus nulla.
+						Praesent habitant suscipit vel nisl vehicula orci.
+						Natoque senectus ridiculus sit proin orci molestie a.
+					</p>
+				</div>
+			</header>
+
+			<main>
+				<section id="awards-section">
+					<img
+						src="../../../public/images/example-award.png"
+						alt=""
+					/>
+					<img
+						src="../../../public/images/example-award.png"
+						alt=""
+					/>
+					<img
+						src="../../../public/images/example-award.png"
+						alt=""
+					/>
+				</section>
+
+				<section id="home-about-section">
+
+					<img
+						src="../../../public/images/maya-angelou.jpg"
+						alt="image of maya angelou"
+					/>
+
+					<div id="home-about-text">
+						<h2>Mona Wright</h2>
+						<h3>Poet, Author</h3>
+						<p>
+							Lorem ipsum odor amet, consectetuer adipiscing elit.
+							Tortor magna tempor velit laoreet massa vivamus
+							nulla. Praesent habitant suscipit vel nisl vehicula
+							orci. Natoque senectus ridiculus sit proin orci
+							molestie a.
+						</p>
+
+						<button>Learn more</button>
+					</div>
+				</section>
+			</main>
+		</>
 	);
 }
 
