f765d6163822dc5a279f4cc889b878eda2192675
MichelleCordovi <michellecordovi@gmail.com>
2024-09-24 20:55:45 -0400

    add underline styling to About heading

---
+++ data.js
@@ -0,0 +1,9 @@
+const poems = [
+	{
+		title: "I Love You",
+        featured: true,
+		poem: "The love I have for you\nMy words cannot express\nAnd sometimes without thinking\nI turn things into a mess\nWhat is it, that makes me act in such a way I do\nKnowing I want ot react\nWith only loving you\nSometimes, I think I'm crazy\nI know you think so too\nBut baby, I'm just daisy\nForsimply loving you.",
+	}
+];
+
+export default poems;
